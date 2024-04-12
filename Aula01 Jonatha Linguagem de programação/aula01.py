0c24031974481b0f8e60dea1b6dc9554a83cbead
JP <jonathapedrozas@gmail.com>
2024-04-11 21:49:14 -0300

    update, python
    fiz a atividade do dia 11/04/2024

---
+++ Aula01 Jonatha Linguagem de programação/aula01.py
@@ -0,0 +1,8 @@
+
+idade = "16"
+
+nome = "jonatha"
+
+hobbies = "jogar basquete!"
+
+print("seu nome é", nome, "e sua idade é", idade, "e você gosta de", hobbies)
---
+++ Aula01 Jonatha Linguagem de programação/aula02.py
@@ -0,0 +1,13 @@
+#print("olá mundo, finalmente quebrei a maldição")
+
+#print serve para printar(exibir) tudo que está dentro do ("") no terminal
+
+#print("olá bem vindo ao nosso app")
+
+nome = input("Jonatha")
+
+idade = input("16")
+
+hobbies = input("Praticar esportes")
+
+print(f"olá {nome}, você tem {idade} anos e seus hobbies são: {hobbies}.")
